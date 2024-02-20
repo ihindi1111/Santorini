7d55635c07209593ca09bf84252265fb51efda81
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-02-19 23:37:33 -0500

    Finalized documentation and state for Player

--- java/main/java/edu/cmu/cs214/hw3/Player.java
+++ java/main/java/edu/cmu/cs214/hw3/Player.java
@@ -1,12 +1,22 @@
 public class Player {
     private Worker[] workers;
 
+    /**
+     * Initalizes a new Player
+     */
     public Player() {
-        // Initialize the workers for the player
         workers = new Worker[]{new Worker(), new Worker()};
     }
 
-    public Worker getWorker(int workerIndex) {
+    /**
+     * Retrieves worker 1 or 2
+     * @param workerIndex
+     * @return
+     */
+    public Worker getWorker(int workerIndex) throws IllegalStateException{
         if (workerIndex == 0 || workerIndex == 1) return workers[workerIndex];
+        else {
+            throw new IllegalStateException("Cannot build beyond the maximum level.");
+        }
     }
 }
