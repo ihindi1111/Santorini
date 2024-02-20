838af1b26d3aaccd8868ea13147282d459a4416c
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-02-19 22:22:02 -0500

    Added tile functions

---
+++ java/main/java/edu/cmu/cs214/hw3/Tile.java
@@ -0,0 +1,19 @@
+public class Tile {
+    private Position position;
+    private boolean isAvailable;
+    private Building hasBuilding;
+    private boolean isOccupied;
+
+    public Tile(Position position) {
+        this.position = position;
+        this.isAvailable = true;
+        this.hasBuilding = null;
+        this.isOccupied = false;
+    }
+
+    public void reset() {
+        this.isAvailable = true;
+        this.hasBuilding = null;
+        this.isOccupied = false;
+    }
+}
