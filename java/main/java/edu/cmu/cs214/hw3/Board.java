8d0b8b32ecea991c27b355de6dc899581a3a2970
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-02-19 23:22:16 -0500

    Added documentation for Board

--- java/main/java/edu/cmu/cs214/hw3/Board.java
+++ java/main/java/edu/cmu/cs214/hw3/Board.java
@@ -1,6 +1,9 @@
 public class Board {
     private Tile[][] tiles;
 
+    /**
+     * Initializes an empty board
+     */
     public Board() {
         tiles = new Tile[5][5]; //5x5 board
         for (int x = 0; x < tiles.length; x++) {
@@ -10,6 +13,9 @@ public class Board {
         }
     }
 
+    /**
+     * Resets the board
+     */
     public void reset() {
         for (Tile[] row : tiles) {
             for (Tile tile : row) {
