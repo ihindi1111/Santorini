5233a98f42b4bca9d07037ec2590acbd3757ccc8
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-08 23:50:46 -0400

    Updated GameFrameworkImpl

--- santorini/src/main/java/edu/cmu/cs214/hw3/santorini/backend/core/GameFrameworkImpl.java
+++ santorini/src/main/java/edu/cmu/cs214/hw3/santorini/backend/core/GameFrameworkImpl.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import edu.cmu.cs214.hw3.santorini.backend.SantoriniPlugin;
+
 /**
  * The framework core implementation.
  */
@@ -14,24 +16,16 @@ public class GameFrameworkImpl implements GameFramework {
     private final int DEFAULT_HEIGHT = 1;
     private final int DEFAULT_WIDTH = 1;
     private String[][] gameGrid;
-    private GamePlugin currentPlugin;
+    private GamePlugin currentPlugin; // Only one plugin, Santorini, will be used.
     private String footer;
-    private List<GamePlugin>  registeredPlugins;
+
 
     public GameFrameworkImpl() {
         gameGrid = new String[DEFAULT_WIDTH][DEFAULT_HEIGHT];
         footer = DEFAULT_FOOTER;
-        registeredPlugins = new ArrayList<GamePlugin>();
-    }
-
-
-
-    /**
-     * Registers a new {@link GamePlugin} with the game framework
-     */
-    public void registerPlugin(GamePlugin plugin) {
-        plugin.onRegister(this);
-        registeredPlugins.add(plugin);
+        currentPlugin = new SantoriniPlugin(); // Ensure this matches the constructor of your SantoriniPlugin.
+        currentPlugin.onRegister(this);
+        // startNewGame(currentPlugin); // Automatically start the Santorini game
     }
 
     /**
@@ -138,10 +132,6 @@ public class GameFrameworkImpl implements GameFramework {
 
     public String getFooter(){
         return footer;
-    }
-
-    public List<String> getRegisteredPluginName(){
-        return registeredPlugins.stream().map(GamePlugin::getGameName).collect(Collectors.toList());
     }
 
     public String getGameOverMsg(){
