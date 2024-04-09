df0b474b94ad34abfd4ef9ba136f74f554c1ca37
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-09 08:44:52 -0400

    Modified Game to have Player stored instead of int

--- santorini/src/main/java/edu/cmu/cs214/hw3/santorini/backend/Game.java
+++ santorini/src/main/java/edu/cmu/cs214/hw3/santorini/backend/Game.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Game {
     private Board board;
     private List<Player> players;
-    private int currentPlayerIndex;
+    private Player currPlayer;
     private boolean gameWon;
 
     public Game() {
@@ -19,13 +19,13 @@ public class Game {
         for (int i = 0; i < 2; i++) {
             players.add(new Player(board));
         }
-        this.currentPlayerIndex = 0;
+        currPlayer = players.get(0);
         this.gameWon = false;
     }
 
     public boolean placeWorker(int workerNum, int x, int y) {
         
-        Player player = players.get(currentPlayerIndex);
+        Player player = currPlayer;
         try {
             player.placeWorker(workerNum, x, y);
             return true;
@@ -37,11 +37,11 @@ public class Game {
     }
 
     public void switchPlayer() {
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+        int nextPlayerIndex = (players.indexOf(currPlayer) + 1) % players.size();
+        this.currPlayer = players.get(nextPlayerIndex);
     }
 
     public boolean checkForWin(int workerID) {
-        Player currPlayer = players.get(currentPlayerIndex);
         return board.getTile(currPlayer.getWorker(workerID).getX(), currPlayer.getWorker(workerID).getY()).getLevel() == 3;
     }
 
@@ -49,15 +49,13 @@ public class Game {
         return gameWon;
     }
 
-    public int getCurrentPlayerIndex() {
-        return currentPlayerIndex;
+    public Player getCurrentPlayer() {
+        return currPlayer;
     }
     
     // Method to move a worker on the board, including checking if the move is valid and if it results in a win.
     public boolean moveWorker(int workerNum, int newX, int newY) {
-        
-        Player player = players.get(currentPlayerIndex);
-        boolean moved = player.moveWorker(newX, newY, workerNum);
+        boolean moved = currPlayer.moveWorker(newX, newY, workerNum);
         if (moved) {
             if (checkForWin(workerNum)) {
                 gameWon = true;
@@ -70,8 +68,7 @@ public class Game {
 
     // Method to build on the board, including checking if the build is valid.
     public boolean build(int workerNum, int buildX, int buildY) {
-        Player player = players.get(currentPlayerIndex);
-        boolean built = player.build(buildX, buildY, workerNum);
+        boolean built = currPlayer.build(buildX, buildY, workerNum);
         if (built) {
             switchPlayer();
         }
