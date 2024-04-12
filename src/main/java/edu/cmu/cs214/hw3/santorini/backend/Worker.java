be1a9269931c593b0cb0c3fd0a7502506fbcbde8
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-12 19:39:46 -0400

    Updated Worker class to setPosition, hold a Player reference and check if they have been placed

--- src/main/java/edu/cmu/cs214/hw3/santorini/backend/Worker.java
+++ src/main/java/edu/cmu/cs214/hw3/santorini/backend/Worker.java
@@ -3,15 +3,17 @@ package edu.cmu.cs214.hw3.santorini.backend;
 public class Worker {
     private int x;
     private int y;
+    private int player;
 
     /**
     * Constructor that initializes a worker at the specified coordinates
     * @param x The initial x-coordinate of the worker
     * @param y The initial y-coordinate of the worker
     */
-    public Worker(int x, int y) {
+    public Worker(int player, int x, int y) {
         this.x = x;
         this.y = y;
+        this.player = player;
     }
 
     /**
@@ -30,19 +32,31 @@ public class Worker {
         return y;
     }
 
-    /**
-    * Sets the x-coordinate of the worker
-    * @param x The new x-coordinate
+     /**
+    * Sets the worker's position on the board.
+    * @param x The new x-coordinate of the worker.
+    * @param y The new y-coordinate of the worker.
     */
-    public void setX(int x) {
+    public void setPosition(int x, int y) {
         this.x = x;
+        this.y = y;
     }
 
     /**
-    * Sets the y-coordinate of the worker
-    * @param y The new y-coordinate
-    */
-    public void setY(int y) {
-        this.y = y;
+     * 
+     * 
+     * @return The player object representing the owner of the worker.
+     */
+    public int getPlayer() {
+        return this.player;
+    }
+
+    /**
+     * Checks if the worker has been placed on the board
+     * @return true if the worker has been placed, false otherwise
+     */
+    public boolean isPlaced() {
+        // A worker is considered placed if its coordinates are not at the initial -1 value
+        return this.x != -1 && this.y != -1;
     }
 }
