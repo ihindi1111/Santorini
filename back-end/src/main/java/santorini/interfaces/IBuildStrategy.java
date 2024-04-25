3f16b6f751110f1cfff3cbf7b09c11fe4f206634
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-24 23:26:28 -0400

    Updated IBuildStrategy

--- back-end/src/main/java/santorini/interfaces/IBuildStrategy.java
+++ back-end/src/main/java/santorini/interfaces/IBuildStrategy.java
@@ -1,5 +1,21 @@
 package interfaces;
 
 public interface IBuildStrategy {
+    /**
+     * Checks if a build operation is valid.
+     * @param worker The worker performing the build.
+     * @param previousTile The tile from which the worker is building.
+     * @param buildTile The tile on which the building is attempted.
+     * @return true if the build is valid, false otherwise.
+     */
+    boolean isValidBuild(Worker worker, Tile previousTile, Tile buildTile);
+
+    /**
+     * Performs the build operation on the specified tile.
+     * @param worker The worker performing the build.
+     * @param previousTile The tile from which the worker is building.
+     * @param buildTile The tile on which the build is being executed.
+     * @return true if the build was successfully performed, false otherwise.
+     */
     boolean performBuild(Worker worker, Tile previousTile, Tile buildTile);
 }
