e2a4ff92e2601ce5871f629e75d9ce02621b005e
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-26 13:05:41 -0400

    Creating a generalized GodCard interface

---
+++ back-end/src/main/java/santorini/interfaces/GodStrategy.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import components.Board;
+import components.Tile;
+import components.Worker;
+import components.Player;
+
+public interface GodStrategy {
+    boolean isValidAction(Player player, Worker worker, Board board, int x, int y);
+    boolean performAction(Player player, Worker worker, Board board, int x, int y);
+    boolean hasPerformedFirstAction();
+    boolean hasSecondAction();
+}
