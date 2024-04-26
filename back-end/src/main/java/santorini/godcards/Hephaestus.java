ca7d536cb2f54923df5f12e969a27d00314764c0
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-26 12:11:08 -0400

    Updated Hephaestus to work properly with build conditions and make methods private in Santorini for better encapsulation

--- back-end/src/main/java/santorini/godcards/Hephaestus.java
+++ back-end/src/main/java/santorini/godcards/Hephaestus.java
@@ -13,7 +13,17 @@ public class Hephaestus implements IBuildStrategy {
     @Override
     public boolean performBuild(Player player, Worker worker, Board board, int x, int y) {
         if (isValidBuild(player, worker, board, x, y)) {  // Only add a block if it won't create a dome
-            previousTile.build();  // Add another block
+            if (!(x == worker.getX() && y == worker.getY())) {
+                board.getTile(x, y).build(); // Only build if it's not a pass
+            }
+            if (!firstBuild) {
+                firstBuild = true;
+                previousTile = board.getTile(x, y);  // Remember the tile of the first build
+            } else {
+                // Reset after optional second build
+                firstBuild = false;
+                previousTile = null;
+            }
             return true;
         }
         return false;
@@ -21,11 +31,27 @@ public class Hephaestus implements IBuildStrategy {
 
     @Override
     public boolean isValidBuild(Player player, Worker worker, Board board, int x, int y) {
-        if (board.getTile(x, y) == previousTile || previousTile == null) {
-            return true; // Skip the second build and end the building phase
-        } //might need fixing
-        else if (x != previousTile.getX() && y != previousTile.getY()) return false;  // Must build on the same tile twice
-        return true;
+        Tile buildTile = board.getTile(x, y);
+    
+        // Check if out of bounds, null tile, or if it's a dome (build not allowed)
+        if (buildTile == null || buildTile.hasDome() ||
+            x < 0 || x >= board.getBOARD_SIZE() ||
+            y < 0 || y >= board.getBOARD_SIZE()) {
+            return false;
+        }
+    
+        // First build check: Must be valid according to board's rules
+        if (!firstBuild) {
+            return board.isValidBuild(player, worker, x, y);
+        } else {
+            // The second build can be a pass if selecting the worker's tile
+            if (x == worker.getX() && y == worker.getY()) {
+                return true; // Passing the second build
+            }
+            
+            // Second build can only be on the same tile as the first build
+            return buildTile == previousTile && !buildTile.hasDome(); // Check it won't create a dome
+        }
     }
 
     public boolean firstBuild() {
