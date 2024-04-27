8ae7d62c3c88fcd659e4fc73bd048f2a8ae61b7c
Itamar Hindi <itamarhindi@Itamars-Air.fios-router.home>
2024-04-26 23:45:17 -0400

    Created TileTesting class

--- back-end/src/test/java/santorini/TileTest.java
+++ back-end/src/test/java/santorini/TileTest.java
@@ -1,71 +1,64 @@
-// package edu.cmu.cs214.hw3.santorini.backend.tests;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
-// import org.junit.jupiter.api.Test;
+import components.Tile;
+import components.Worker;
 
-// import edu.cmu.cs214.hw3.santorini.backend.Tile;
-// import edu.cmu.cs214.hw3.santorini.backend.Worker;
+public class TileTest {
+    private Tile tile;
+    private Worker worker;
 
-// import org.junit.Before;
-// import static org.junit.jupiter.api.Assertions.*;
+    @Before
+    public void setUp() {
+        tile = new Tile(0, 0);
+        worker = new Worker(1, 0, 0); // Assuming Worker constructor accepts playerID, x, y
+    }
 
-// public class TileTest {
+    @Test
+    public void testInitialConditions() {
+        assertFalse("Newly created tile should not have a dome", tile.hasDome());
+        assertEquals("Newly created tile should have level 0", 0, tile.getLevel());
+        assertNull("Newly created tile should not have a worker", tile.getWorker());
+    }
 
-//     private Tile tile;
+    @Test
+    public void testSetWorker() {
+        tile.setWorker(worker);
+        assertTrue("Tile should be occupied after setting worker", tile.isOccupied());
+        assertEquals("Tile should return the correct worker", worker, tile.getWorker());
+    }
 
-//     @Before
-//     public void setUp() {
-//         tile = new Tile(0, 0);
-//     }
+    @Test
+    public void testBuildIncreasesLevel() {
+        tile.build();
+        assertEquals("Building on a tile should increase its level by 1", 1, tile.getLevel());
+        assertFalse("Building once should not automatically add a dome", tile.hasDome());
+    }
 
-//     @Test
-//     public void testInitialLevel() {
-//         assertEquals(0, tile.getLevel());
-//     }
+    @Test
+    public void testBuildAddsDomeAtFourthLevel() {
+        for (int i = 0; i < 3; i++) {
+            tile.build();
+        }
+        assertTrue("Building four times should add a dome", tile.build());
+        assertTrue("Tile should have a dome after fourth build", tile.hasDome());
+    }
 
-//     @Test
-//     public void testIncreaseLevel() {
-//         tile.increaseLevel();
-//         assertEquals(1, tile.getLevel());
-//     }
+    @Test
+    public void testNoBuildWhenDomeExists() {
+        for (int i = 0; i < 4; i++) {
+            tile.build();
+        }
+        assertFalse("Should not be able to build once dome is placed", tile.build());
+    }
 
-//     @Test
-//     public void testIncreaseLevelUpToDome() {
-//         for (int i = 0; i < 4; i++) {
-//             tile.increaseLevel();
-//         }
-//         assertTrue(tile.hasDome());
-//         assertEquals(3, tile.getLevel());
-//     }
-
-//     @Test
-//     public void testOccupiedStatus() {
-//         tile.setOccupied(true);
-//         assertTrue(tile.isOccupied());
-//     }
-
-//     @Test
-//     public void testNotOccupiedStatus() {
-//         tile.setOccupied(false);
-//         assertFalse(tile.isOccupied());
-//     }
-
-//     @Test
-//     public void testBuildIncreasesLevel() {
-//         tile.build();
-//         assertEquals(1, tile.getLevel());
-//     }
-
-//     @Test
-//     public void testMoveWorkerChangesOccupancy() {
-//         Worker worker = new Worker(0, 1); // Assuming Worker constructor Worker(int x, int y)
-//         Tile workerTile = new Tile(0, 1);
-//         workerTile.setOccupied(true);
-
-//         tile.moveWorker(worker, workerTile);
-
-//         assertEquals(tile.getX(), worker.getX());
-//         assertEquals(tile.getY(), worker.getY());
-//         assertTrue(tile.isOccupied());
-//         assertFalse(workerTile.isOccupied());
-//     }
-// }
+    @Test
+    public void testMoveWorkerTo() {
+        Tile destinationTile = new Tile(0, 1);
+        tile.setWorker(worker);
+        tile.moveWorkerTo(destinationTile);
+        assertTrue("Destination tile should have the worker after moving", destinationTile.isOccupied());
+        assertNull("Original tile should not have the worker after moving", tile.getWorker());
+    }
+}
